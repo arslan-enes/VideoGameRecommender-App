0438bb62cd89fb7b590aabcf572e582d373ba0e0
arslan-enes <arslanenes515@gmail.com>
2023-06-08 17:51:02 +0300

    change mongodb connection

--- Recommender/main/pyscripts/pymongo_get_database.py
+++ Recommender/main/pyscripts/pymongo_get_database.py
@@ -3,15 +3,22 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+
+try: 
+    load_dotenv()
+    uri = os.getenv("MONGODB_CONNECTION_STRING")
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    video_games = client["VideoGames"]
+    print("Connected to MongoDB Atlas cluster!")
+except Exception as e:
+    print("Error connecting to MongoDB Atlas cluster:\n", e)
+
+
 
 
 def get_database():
-    uri = os.getenv("MONGODB_CONNECTION_STRING")
     # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
-
-    return client['VideoGames']
+    return video_games
 
 if __name__ == "__main__":
     db_name = get_database()
